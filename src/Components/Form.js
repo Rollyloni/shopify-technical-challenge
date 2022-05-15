a9f4d5ea31a62033123e299fb2008c360679d796
Rolake Olonisakin <rollyloni@gmail.com>
2022-05-15 14:35:30 -0700

    final commit

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -29,7 +29,7 @@ class Form extends Component {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.API__KEY}`,
+        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
       },
       body: JSON.stringify(data),
     })
